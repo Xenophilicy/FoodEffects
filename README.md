fbf4569dae8601b3a7455ba45d3273526bc55550
Xeno <xenophilicy@gmail.com>
2020-02-08 14:38:00 -0600

    Update v1.1.0
    Add support for non-consumable items. Converted JSON item listing to YAML for easier formatting.

--- README.md
+++ README.md
@@ -6,48 +6,49 @@
 # [![Xenophilicy](https://file.xenoservers.net/Resources/GitHub-Resources/foodeffects.png)]()
 
 ## Information
-With this plugin, you are able to assign different effects to items so that when a player consumes that item, they are given the effect for a set duration at a set effect level! All of these options can be set in the *items.json* file. The format for the item listing can be found in both the *config.yml* file and down below if you ever get confused. You can also find examples inside the default *items.json* file. If you would like a video example of the plugin in action, click [here](https://youtu.be/SbITnMk8jVE)
+With this plugin, you are able to assign different effects to items so that when a player interacts with or consumes that item, they are given the effect for a set duration at a set effect level! All of these options can be set in the *items.json* file. The format along with some examples for the item listing can be found in both the *config.yml* file and down below if you ever get confused. If you would like a video example of the plugin in action, click [here](https://youtu.be/SbITnMk8jVE)
 
 ## Item Formatting
-```
-{
-    "Items":{
-        "Item-ID":[
-            [Effect-ID,Effect-Amplifier,Effect-Duration]
-        ]
-    }
-}
+```yaml
+"Item-ID:Item-Damage":
+    - [Effect-ID,Effect-Amplifier,Effect-Duration]
+    - [Effect-ID,Effect-Amplifier,Effect-Duration]
+    - [Effect-ID,Effect-Amplifier,Effect-Duration]
 ```
 
 ## Item Listing Example
-
 Desired configuration:
-* When steak(364) is consumed, give blindness(15) level 1 for 10 seconds and strength(5) level 2 for 30 seconds
-* When an apple(260) is consumed, give speed(1) level 3 for 15 seconds and infinite(0) night vision(16) level 1
+* When steak(ID = 364) is consumed, give blindness(ID = 15) level 1 for 10 seconds and strength(ID = 5) level 2 for 30 seconds
+* When an apple(ID = 260) is consumed, give speed(ID = 1) level 3 for 15 seconds and night vision(ID = 16) level 1 infinitely(0)
+* When interacting with a jungle sapling(ID = 6:3), give slowness(ID = 2) level 1 for 60 seconds, fire resistance(ID = 12) level 1 for 30 seconds, and peed(ID = 1) level 3 for 5 seconds
+* When interacting with a red mushroom(ID = 40), give slowness(ID = 2) level 1 for 5 seconds
 
-Configuration in JSON:
-```json
-{
-    "Items":{
-        "364":[
-            [15,1,10],
-            [5,2,30]
-        ],
-        "260":[
-            [1,3,15],
-            [16,1,0]
-        ]
-    }
-}
+Configuration in config.yml:
+```yaml
+Consumables:
+  "364":
+    - [15,1,10]
+    - [5,2,30]
+  "260":
+    - [1,3,15]
+    - [16,1,0]
+
+Non-Consumables:
+  "6:3": 
+    - [2,1,60]
+    - [12,1,30]
+    - [1,3,5]
+  "40":
+    - [2,1,5]
 ```
 
 ***
 
 ## FoodEffects Details
 * **API:** 3.0.0+
-* **Version:** 1.0.0
-* **Basic Description:** Enables customizable effects for consuming items
-* *Easy to edit items.json file*
+* **Version:** 1.1.0
+* **Basic Description:** Enables customizable effects for interacting with and consuming items
+* *Easy to edit config.yml file*
 * *Simple code for editing and debugging*
 ***
 
--- src/Xenophilicy/FoodEffects/FoodEffects.php
+++ src/Xenophilicy/FoodEffects/FoodEffects.php
@@ -18,42 +18,68 @@ namespace Xenophilicy\FoodEffects;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
-use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\player\{PlayerItemConsumeEvent,PlayerInteractEvent};
 use pocketmine\entity\{Effect,EffectInstance};
 
 class FoodEffects extends PluginBase implements Listener {
 
     private $config;
+    private $consumables;
+    private $nonConsumables;
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveResource("config.yml");
-        $this->saveResource("items.json");
         $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
-        $this->json = new Config($this->getDataFolder()."items.json", Config::JSON);
         $this->getLogger()->info("FoodEffects has been enabled!");
         $version = $this->config->get("VERSION");
-        if($version != "1.0.0"){
-            $this->getLogger()->warning("You have updated FoodEffects but have an invalid config version!");
+        if($version != "1.1.0"){
+            $this->getLogger()->critical("You have updated FoodEffects but have an outdated config! Please delete your old config to continue using FoodEffects!");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return;
         }
-        $this->list = $this->json->get("Items");
-        foreach($this->list as $key => $values){
-            if(!is_numeric($key)){
-                $this->getLogger()->warning("Invalid item ID found, must be a numeric value! Invalid item ID: $key");
-            }
-            foreach($values as $value){
-                foreach($value as $test){
-                    if(!is_numeric($test)){
-                        $this->getLogger()->warning("Invalid effect string found, all effect related arguments must be numerical! Invalid argument: ".$test);
-                    }
+        $oldFile = $this->getDataFolder()."items.json";
+        if(file_exists($oldFile)){
+            $this->getLogger()->critical("You have updated FoodEffects but are still using an outdated file. Please tranfer your entries from 'items.json' to the new config file using the provided format to continue using FoodEffects!");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return;
+        }
+        $this->consumables = $this->config->get("Consumables");
+        $this->nonConsumables = $this->config->get("Non-Consumables");
+        $itemGroups = [$this->consumables,$this->nonConsumables];
+        foreach($itemGroups as $itemGroup){
+            foreach($itemGroup as $key => $effects){
+                if(!is_numeric($key) && strpos($key, ":") > 1){
+                    $this->getLogger()->warning("Invalid item ID found! Invalid item ID: ".$key." not supported, disabling plugin!");
+                    $this->getServer()->getPluginManager()->disablePlugin($this);
+                    return;
                 }
-                if(!is_numeric($value[0]) || !is_numeric($value[1]) || !is_numeric($value[2])){
-                    if($value[0] <= 26 && $value[0] >= 1){
-                        if(!$value[1] <= 255){
-                            $this->getLogger()->warning("Invalid effect amplifier found, must be no greater than 255! Invalid effet amplifier: ".$value[1]);
+                foreach($effects as $effValues){
+                    $subscripts = [0,1,2];
+                    foreach($subscripts as $subscript){
+                        if(!isset($effValues[$subscript])){
+                            $this->getLogger()->warning("Invalid effect string found, you must have 3 values for each effect. Effect for item ID: ".$key." is invalid, disabling plugin!");
+                            $this->getServer()->getPluginManager()->disablePlugin($this);
+                            return;
                         }
-                    } else{
-                        $this->getLogger()->warning("Invalid effect ID found, please see the ID list in the config.yml! Invalid effect: ".$value[0]);
+                    }
+                    if(!is_numeric($effValues[0])){
+                        $this->getLogger()->warning("Invalid effect string found, all effect related arguments must be numerical! Invalid argument: ".$value[0]." not supported, disabling plugin!");
+                        $this->getServer()->getPluginManager()->disablePlugin($this);
+                        return;
+                    }
+                    if(!is_numeric($effValues[0]) || !is_numeric($effValues[1]) || !is_numeric($effValues[2])){
+                        if($effValues[0] <= 26 && $effValues[0] >= 1){
+                            if(!$effValues[1] <= 255){
+                                $this->getLogger()->warning("Invalid effect amplifier found, must be no greater than 255! Effect amplifier: ".$effValues[1]." not supported, disabling plugin!");
+                                $this->getServer()->getPluginManager()->disablePlugin($this);
+                                return;
+                            }
+                        } else{
+                            $this->getLogger()->warning("Invalid effect ID found, please see the ID list in the config.yml! Effect: ".$effValues[0]." not found, disabling plugin!");
+                            $this->getServer()->getPluginManager()->disablePlugin($this);
+                            return;
+                        }
                     }
                 }
             }
@@ -63,11 +89,30 @@ class FoodEffects extends PluginBase implements Listener {
     public function onItemConsume(PlayerItemConsumeEvent $event){
         $player = $event->getPlayer();
         $item = $event->getItem();
-        foreach($this->list as $key => $values){
-            if($item->getId() == (int)$key){
-                foreach($values as $value){
-                    $effect = new EffectInstance(Effect::getEffect($value[0]));
-                    $player->addEffect($effect->setAmplifier($value[1])->setDuration($value[2]*20));
+        $idFull = $item->getId().":".$item->getDamage();
+        $id = $item->getId();
+        if($player->getFood() < 20){
+            foreach($this->consumables as $key => $effects){
+                if($idFull == (string)$key || $id == (string)$key){
+                    foreach($effects as $effValues){
+                        $effectInstance = new EffectInstance(Effect::getEffect($effValues[0]));
+                        $player->addEffect($effectInstance->setAmplifier($effValues[1])->setDuration($effValues[2]*20));
+                    }
+                }
+            }
+        }
+    }
+
+    public function onItemInteract(PlayerInteractEvent $event){
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+        $idFull = $item->getId().":".$item->getDamage();
+        $id = $item->getId();
+        foreach($this->nonConsumables as $key => $effects){
+            if($idFull == (string)$key || $id == (string)$key){
+                foreach($effects as $effValues){
+                    $effectInstance = new EffectInstance(Effect::getEffect($effValues[0]));
+                    $player->addEffect($effectInstance->setAmplifier($effValues[1])->setDuration($effValues[2]*20));
                 }
             }
         }
